1d68f60a81d8bc50f92deeabbd7e92dc500fefae
Thomas Raulet <raulet.thomas@gmail.com>
2017-03-15 00:08:47 +0100

    WIP : ex7 step1

--- machine-learning-ex7/ex7/findClosestCentroids.m
+++ machine-learning-ex7/ex7/findClosestCentroids.m
@@ -1,7 +1,7 @@
 function idx = findClosestCentroids(X, centroids)
 %FINDCLOSESTCENTROIDS computes the centroid memberships for every example
 %   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
-%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   in idx for a dataset X where each row is a single example. idx = m x 1
 %   vector of centroid assignments (i.e. each entry in range [1..K])
 %
 
@@ -15,12 +15,25 @@ idx = zeros(size(X,1), 1);
 % Instructions: Go over every example, find its closest centroid, and store
 %               the index inside idx at the appropriate location.
 %               Concretely, idx(i) should contain the index of the centroid
-%               closest to example i. Hence, it should be a value in the 
+%               closest to example i. Hence, it should be a value in the
 %               range 1..K
 %
 % Note: You can use a for-loop over the examples to compute this.
 %
+m = size(X,1);
+for i=1:m
+% iterate through m exemples
 
+  tmp_distortion = 10000000;
+  for j=1:K
+    % test all centroids
+    k_dist = norm(X(i,:) - centroids(K,:)) ^ 2;
+    if k_dist < tmp_distortion
+      idx(i) = j;
+      tmp_distortion = k_dist;
+    end
+  end
+end
 
 
 
@@ -30,4 +43,3 @@ idx = zeros(size(X,1), 1);
 % =============================================================
 
 end
-
